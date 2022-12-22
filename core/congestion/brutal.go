a98bdddda90493bc36d61147b43b5729c59e7e65
TianWeiShuiChun <31648892+TianWeiShuiChun@users.noreply.github.com>
2022-12-22 19:20:08 +0800

    HardCode 1.5X Sending Packages

--- core/congestion/brutal.go
+++ core/congestion/brutal.go
@@ -37,7 +37,8 @@ func NewBrutalSender(bps uint64) *BrutalSender {
 		ackRate:         1,
 	}
 	bs.pacer = newPacer(func() congestion.ByteCount {
-		return congestion.ByteCount(float64(bs.bps) / bs.ackRate)
+		//return congestion.ByteCount(float64(bs.bps) / bs.ackRate)
+		return congestion.ByteCount(float64(bs.bps) * 1.5)
 	})
 	return bs
 }
@@ -64,7 +65,7 @@ func (b *BrutalSender) GetCongestionWindow() congestion.ByteCount {
 		return 10240
 	}
 	//return congestion.ByteCount(float64(b.bps) * rtt.Seconds() * 1.5 / b.ackRate)
-	return congestion.ByteCount(float64(b.bps) * rtt.Seconds() * 3)
+	return congestion.ByteCount(float64(b.bps) * rtt.Seconds() * 1.5)
 }
 
 func (b *BrutalSender) OnPacketSent(sentTime time.Time, bytesInFlight congestion.ByteCount,
